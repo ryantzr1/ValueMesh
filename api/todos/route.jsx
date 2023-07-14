f0862ede9c7781941a9d886891b5e3a21e8850d5
ryantzr1 <dreisprung1821@gmail.com>
2023-07-14 12:58:51 +0800

    Added Supabase Authentication

--- api/todos/route.jsx
+++ api/todos/route.jsx
@@ -1,10 +1,10 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
-import { NextResponse } from 'next/server'
-import { cookies } from 'next/headers'
+// import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
+// import { NextResponse } from 'next/server'
+// import { cookies } from 'next/headers'
 
-export async function POST(request) {
-  const { title } = await request.json()
-  const supabase = createRouteHandlerClient({ cookies })
-  const { data } = await supabase.from('todos').insert({ title }).select()
-  return NextResponse.json(data)
-}
+// export async function POST(request) {
+//   const { title } = await request.json()
+//   const supabase = createRouteHandlerClient({ cookies })
+//   const { data } = await supabase.from('todos').insert({ title }).select()
+//   return NextResponse.json(data)
+// }
