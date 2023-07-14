aa1f72705fb1e8632a4b7811bcd1bedaf90d077f
ryantzr1 <dreisprung1821@gmail.com>
2023-07-14 13:04:52 +0800

    Added Supabase Authentication

--- src/auth/callback/route.js
+++ src/auth/callback/route.js
@@ -1,14 +1,10 @@
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
-
 export async function GET(request) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
 
   if (code) {
     console.log(code + " this is the code");
-    const supabase = createRouteHandlerClient({ cookies });
+    const supabase = createRouteHandlerClient({ cookies: request.cookies });
     await supabase.auth.exchangeCodeForSession(code);
   }
 
