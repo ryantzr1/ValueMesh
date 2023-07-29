f47543d2b89116a4e9bb3635eb7805cef82ad434
ryantzr1 <dreisprung1821@gmail.com>
2023-07-29 23:51:11 +0800

    Change Shareable Link update

--- src/pages/contact/[id].js
+++ src/pages/contact/[id].js
@@ -8,6 +8,7 @@ import mongoose from "mongoose";
 
 const Contact = ({ id }) => {
   const [person, setPerson] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   const { loading, user } = useContext(UserContext);
   const router = useRouter();
 
@@ -27,6 +28,7 @@ const Contact = ({ id }) => {
           response.data.data.notes = "";
 
           setPerson(response.data.data);
+          setIsLoading(false);
         } catch (error) {
           console.error(error);
         }
@@ -46,20 +48,20 @@ const Contact = ({ id }) => {
     }
   };
 
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div className={styles.container}>
-      {person ? (
-        <div className="flex flex-col items-center">
-          <SharedPersonCard person={person} />
-          {!user && (
-            <button className={styles.button} onClick={handleAddToDashboard}>
-              Sign In to Add to Dashboard
-            </button>
-          )}
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
+      <div className="flex flex-col items-center">
+        <SharedPersonCard person={person} />
+        {!user && (
+          <button className={styles.button} onClick={handleAddToDashboard}>
+            Sign In to Add to Dashboard
+          </button>
+        )}
+      </div>
     </div>
   );
 };
